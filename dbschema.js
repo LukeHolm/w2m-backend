a2cbf55e8649631424d635754506395b66d04871
Lucas <lucas.e.holm@gmail.com>
2022-01-03 11:13:34 +0100

    first commit

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    posts: [
+        {
+            userHandle: 'user',
+            body: 'This is the post body',
+            createdAt: '2022-01-02T12:56:36.018Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
